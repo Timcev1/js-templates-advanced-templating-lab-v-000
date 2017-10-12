16761f414b0a14eb4af04b3b771184dded07db9e
Timothy Cevallos <timcev@gmail.com>
2017-10-12 23:58:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+function init() {
+  //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+}
+
 function createRecipe() {
   var recipe = getRecipeVals()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
@@ -49,14 +57,6 @@ function handlebarsSetup() {
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 }
 
-
-function init() {
-  //put any page initialization/handlebars initialization here
-  handlebarsSetup()
-  var formTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(formTemplate)
-  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
-}
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
