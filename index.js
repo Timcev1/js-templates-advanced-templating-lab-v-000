8a9b6e576127f70157972643af0f33b1626dae3c
Timothy Cevallos <timcev@gmail.com>
2017-10-12 23:50:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 function init() {
-  //put any page initialization/handlebars initialization here
   handlebarsSetup()
-  initForm()
-}
-function initForm() {
-  var formTemplate= document.getElementById("recipe-form-template").innerHTML
-  var template= Handlebars.compile(formTemplate)
-  document.getElementsByTagName("main")[0].innerHTML= template({'submitAction': 'createRecipe()'})
+  //put any page initialization/handlebars initialization here
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 
 function handlebarsSetup() {
