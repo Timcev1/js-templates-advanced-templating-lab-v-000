d91e9a021f6073cbfd9819031cd7823390156a75
Timothy Cevallos <timcev@gmail.com>
2017-10-12 23:56:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -58,9 +58,9 @@ function handlebarsSetup() {
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
-    var formTemplate = document.getElementById("recipe-form-template").innerHTML
-    var template = Handlebars.compile(formTemplate)
-    document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
