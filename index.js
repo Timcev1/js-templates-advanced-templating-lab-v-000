021fe6a3577f0c2c700eadd491fadf0f95557ed0
Timothy Cevallos <timcev@gmail.com>
2017-10-12 23:50:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function init() {
   handlebarsSetup()
   initForm()
 }
-+function initForm() {
-+  var formTemplate= document.getElementById("recipe-form-template").innerHTML
-+  var template= Handlebars.compile(formTemplate)
-+  document.getElementsByTagName("main")[0].innerHTML= template({'submitAction': 'createRecipe()'})
-+}
+function initForm() {
+  var formTemplate= document.getElementById("recipe-form-template").innerHTML
+  var template= Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML= template({'submitAction': 'createRecipe()'})
+}
 
 function handlebarsSetup() {
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
